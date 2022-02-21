78f510ebd98dd53aea7d2312367d00253dca7745
Merlin Zuni <merlin.zuni@gmail.com>
2022-02-21 11:08:33 +0100

    update to js code

--- sketch-slashes.js
+++ sketch-slashes.js
@@ -1,8 +1,9 @@
-let slashes;
-let angle = 0;
-let song;
-let amp;
-let button;
+// Project variables
+let slashes; // liip icon logo
+let angle = 0; //Spin amount
+let song; // Audio object
+let amp; // Sound data value 
+let button; // play pause button
 
 function toggleSong(){
     if (song.isPlaying()){
@@ -13,7 +14,9 @@ function toggleSong(){
 }
 
 function preload() {
+    // loading files into the document
     slashes = loadModel('src/liip-slashes.obj'); // 3d model from blender
+    // slashes = loadModel('src/boombox.obj');
     song = loadSound('src/Tribe-Called-Quest-Luck-Of-Lucien.mp3'); // audio file
 }
 
@@ -21,7 +24,7 @@ function setup() {
 	createCanvas(windowWidth, windowHeight-100, WEBGL);
     button = createButton('play/pause');
     button.mousePressed(toggleSong);
-    song.play();
+    //song.play();
     amp = new p5.Amplitude();
 
   }
@@ -37,13 +40,22 @@ function setup() {
 
     background(110, 166, 68); // liip green 700
 
-
     // Scene Lights
     pointLight(208, 221, 44, -windowWidth/2, 0, 0) // liip green 100 - side
 	pointLight(164, 195, 57, windowWidth/2, 0, 0)  // liip green 400 -side
     pointLight(208, 221, 44, 0, -200, 0)  // liip green 100 - above 
     pointLight(164, 195, 57, 0, 300, 0)  // liip green 400 - below 
     pointLight(255, 255, 255, 0, 0, 300)  // white 
+
+    // stroke(0);
+    // rotateY(-angle);
+    // specularMaterial(255, 255, 0); // fill object with white
+    // torus(200, 15);
+
+    // stroke(0);
+    // rotateX(angle);
+    // normalMaterial(255, 255, 0); // fill object with white
+    // torus(300, 25);
 
     // spin object
     rotateX(angle);
@@ -52,13 +64,12 @@ function setup() {
 
     // slash object settings
     noStroke();
-    scale(80+volWave); // size - move with music
+    scale(100+volWave); // size - move with music
    	ambientMaterial(255); // fill object with white
     //normalMaterial(); // rainbow debugger color 
     model(slashes); // load 3d model into canvas
-
+    
     // value for the rotation
 	angle += 0.01
-    frameRate(fps)
   }
   
